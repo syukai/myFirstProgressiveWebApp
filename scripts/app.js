fec987919846114da5f6e24b36c0a5385dd3a0bf
syukai <d_hirayama@rapid.ocn.ne.jp>
2017-11-13 22:00:51 +0900

    no comment

--- scripts/app.js
+++ scripts/app.js
@@ -369,7 +369,7 @@
   // TODO add service worker code here
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('./service-worker.js')
+             .register('/myFirstProgressiveWebApp/service-worker.js')
              .then(function() { console.log('Service Worker Registered'); });
   }
 })();
